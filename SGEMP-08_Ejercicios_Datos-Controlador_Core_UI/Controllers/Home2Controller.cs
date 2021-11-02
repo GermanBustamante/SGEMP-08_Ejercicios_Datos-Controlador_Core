4dd4624f4fc7948a459a1ace8388eb0b3f014769
Germán De Bustamante Conde <germanbustamante.trabajo@gmail.com>
2021-11-02 01:11:30 +0100

    Agregar archivos de proyecto.

---
+++ SGEMP-08_Ejercicios_Datos-Controlador_Core_UI/Controllers/Home2Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGEMP_08_Ejercicios_Datos_Controlador_Core_UI.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index2(string nombre)
+        {
+            ViewBag.Nombre = nombre;
+            return View();
+        }
+    }
+}
---
+++ SGEMP-08_Ejercicios_Datos-Controlador_Core_UI/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGEMP_08_Ejercicios_Datos_Controlador_Core_UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Saludo(String nombre)
+        {
+            ViewBag.Nombre = nombre;//TODO PREGUNTAR RUTAS
+            return View();
+        }
+    }
+}
